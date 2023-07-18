8351cff55141d334b029f13e3950747cc246994b
jacute <Eri542@yandex.ru>
2023-07-18 22:08:44 +0700

    add config

---
+++ config/config.py
@@ -0,0 +1,22 @@
+CATEGORIE = 'https://www2.hm.com/pl_pl/ona/produkty/stroje-kapielowe.html?sort=stock&image-size=small&image=model&offset=0&page-size=500'
+# ?sort=stock&image-size=small&image=model&offset=0&page-size=500
+TIMEOUT = 0.5
+SAVE_PHOTO_PATH = 'photo/'
+HOST = '85.193.92.123'
+SAVE_XLSX_PATH = 'xlsx/'
+TEMPLATE_NAME = 'Купальный костюм.xlsx'
+PARSE_LIMIT = 200
+PRICE_TABLE = {
+    "КУРС_USD_ЗЛОТЫ":"4.08",
+    "КУРС_USD_RUB":"88.2",
+    "КУРС_БЕЛ.РУБ_РУБ":"29.28",
+    "КУРС_EUR_БЕЛ.РУБ":"3.35",
+
+    "ЦЕНА_ДОСТАВКИ_В_КАТЕГОРИИ":"4",
+    "КОЭФ_КОНВЕРТАЦИИ":"1.025",
+    "СРЕД_ЦЕН_ДОСТАВКИ":"300",
+    "НАЦЕНКА":"1.47",
+    "ПРОЦЕНТЫ_ОЗОН":"0.18",
+    "ПРОЦЕНТЫ_НАЛОГ":"0.06",
+    "ПРОЦЕНТЫ_ЭКВАЙРИНГ":"0.0115"
+}
